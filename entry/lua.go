559d0306fc9dd1812b9480ae0f1841f57d4d2081
jefurry <jefurry@qq.com>
2018-02-01 17:37:49 +0800

    lua state pool, lua package search path and testing

---
+++ entry/lua.go
@@ -0,0 +1,155 @@
+// (c) 2018, Jeff Chen <jefurry@qq.com>
+//
+// This file is part of Feige
+//
+// Feige is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Feige is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Feige.  If not, see <http://www.gnu.org/licenses/>.
+
+package entry
+
+import (
+	"errors"
+	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"github.com/yuin/gopher-lua"
+	"os"
+	"strings"
+	"sync"
+)
+
+const (
+	// lua state pool size default to 10
+	LUA_STATE_POOL_SIZE = 10
+)
+
+var (
+	OldLuaPathDefault   = lua.LuaPathDefault
+	ErrLuaStatePoolFull = errors.New("lua state pool fulled")
+)
+
+type (
+	NewFunc func(l *lua.LState) error
+)
+
+func SetLuaPath(p string) error {
+	lua.LuaPathDefault = ""
+
+	return AddLuaPath(p)
+}
+
+// add one lua path search to LuaPathDefault
+func AddLuaPath(p string) error {
+	if p == "" {
+		return nil
+	}
+
+	p, err := homedir.Expand(p)
+	if err != nil {
+		return err
+	}
+
+	var prefix = ""
+	if lua.LuaPathDefault != "" {
+		prefix = lua.LuaPathDefault + ";"
+	}
+
+	p = strings.TrimRight(p, "/")
+	p = strings.TrimRight(p, "\\")
+
+	ps := string(os.PathSeparator)
+	p = fmt.Sprintf("%s%s%s?.lua;%s%s?%sinit.lua", prefix, p, ps, p, ps, ps)
+	lua.LuaPathDefault = p
+
+	return nil
+}
+
+type LuaStatePool struct {
+	ls        []*lua.LState
+	m         sync.Mutex
+	size      int
+	makedSize int
+	whenNew   NewFunc
+}
+
+func (lp *LuaStatePool) NewLuaStatePool(whenNew NewFunc, size int) *LuaStatePool {
+	if size <= 0 {
+		size = LUA_STATE_POOL_SIZE
+	}
+	lp.size = size
+
+	return &LuaStatePool{
+		ls:      make([]*lua.LState, size),
+		whenNew: whenNew,
+	}
+}
+
+func (lp *LuaStatePool) New() (*lua.LState, error) {
+	lp.m.Lock()
+	defer lp.m.Unlock()
+
+	lp.makedSize = lp.makedSize + 1
+	l := lua.NewState()
+
+	// setting the Lua State up here.
+	// load scripts, set global variables, share channels, etc...
+	if lp.whenNew != nil {
+		if err := lp.whenNew(l); err != nil {
+			return nil, err
+		}
+	}
+
+	return l, nil
+}
+
+// get one lua state from pool
+func (lp *LuaStatePool) Get() (*lua.LState, error) {
+	lp.m.Lock()
+	defer lp.m.Unlock()
+
+	n := len(lp.ls)
+	if n == 0 {
+		if lp.makedSize >= lp.size {
+			return nil, ErrLuaStatePoolFull
+		}
+
+		return lp.New()
+	}
+
+	l := lp.ls[n-1]
+	lp.ls = lp.ls[0 : n-1]
+
+	return l, nil
+}
+
+// put lua state into pool
+func (lp *LuaStatePool) Put(l *lua.LState) {
+	lp.m.Lock()
+	defer lp.m.Unlock()
+
+	lp.ls = append(lp.ls, l)
+}
+
+func (lp *LuaStatePool) Close(l *lua.LState) {
+	lp.m.Lock()
+	defer lp.m.Unlock()
+
+	l.Close()
+	lp.makedSize = lp.makedSize - 1
+}
+
+// close all lua state
+func (lp *LuaStatePool) Shutdown() {
+	for _, l := range lp.ls {
+		lp.Close(l)
+	}
+}
---
+++ entry/lua_test.go
@@ -0,0 +1,59 @@
+// (c) 2018, Jeff Chen <jefurry@qq.com>
+//
+// This file is part of Feige
+//
+// Feige is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Feige is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Feige.  If not, see <http://www.gnu.org/licenses/>.
+
+package entry
+
+import (
+	"github.com/yuin/gopher-lua"
+	"testing"
+)
+
+func TestLuaPath(t *testing.T) {
+	if err := SetLuaPath(""); err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	if lua.LuaPathDefault != "" {
+		t.Fatalf("LuaPathDefault mismatching")
+	}
+
+	if err := SetLuaPath("~/.feige/modules"); err != nil {
+		t.Fatalf(err.Error())
+	}
+	if lua.LuaPathDefault != "/Users/jefurry/.feige/modules/?.lua;/Users/jefurry/.feige/modules/?/init.lua" {
+		t.Fatalf("LuaPathDefault mismatching")
+	}
+
+	if err := AddLuaPath("~/.feige/lublibs"); err != nil {
+		t.Fatalf(err.Error())
+	}
+	if lua.LuaPathDefault != "/Users/jefurry/.feige/modules/?.lua;/Users/jefurry/.feige/modules/?/init.lua;/Users/jefurry/.feige/lublibs/?.lua;/Users/jefurry/.feige/lublibs/?/init.lua" {
+		t.Fatalf("LuaPathDefault mismatching")
+	}
+
+	lua.LuaPathDefault = OldLuaPathDefault
+	if lua.LuaPathDefault != "./?.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua" {
+		t.Fatalf("LuaPathDefault mismatching")
+	}
+
+	if err := AddLuaPath("~/.feige/modules"); err != nil {
+		t.Fatalf(err.Error())
+	}
+	if lua.LuaPathDefault != "./?.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua;/Users/jefurry/.feige/modules/?.lua;/Users/jefurry/.feige/modules/?/init.lua" {
+		t.Fatalf("LuaPathDefault mismatching")
+	}
+}
