3a336df9812baaefa89c6d933595d89243fdda0a
jefurry <jefurry@qq.com>
2018-02-02 01:15:48 +0800

    add ip utils

---
+++ utils/ip.go
@@ -0,0 +1,34 @@
+// (c) 2018, Jeff Chen <jefurry@qq.com>
+//
+// This file is part of Feige
+//
+// Feige is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Feige is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Feige.  If not, see <http://www.gnu.org/licenses/>.
+
+package utils
+
+import (
+	"net"
+)
+
+func IsIPV4(ip string) bool {
+	if net.ParseIP(ip).To4() != nil {
+		return true
+	}
+
+	return false
+}
+
+func IsIPV6(ip string) bool {
+	return !IsIPV4(ip)
+}
